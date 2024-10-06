52999229492ecf2b701045b7deb4611a19119c96
ruochen yin <772602092@qq.com>
2024-10-06 22:01:22 +0800

    attention

---
+++ UccDL/multiheadAttention.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % num_heads == 0
+
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.depth = d_model // num_heads
+
+        self.wq = nn.Linear(d_model, d_model)
+        self.wk = nn.Linear(d_model, d_model)
+        self.wv = nn.Linear(d_model, d_model)
+
+        self.dense = nn.Linear(d_model, d_model)
